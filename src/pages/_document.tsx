ea20363b46b6aed8168bcbbc640ad961f1af8349
massaaki <eu@massaaki.com>
2022-06-12 14:50:52 -0300

    refactor: remove unnecessary code in _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -29,17 +29,4 @@ export default class MyDocument extends Document {
 			sheet.seal();
 		}
 	}
-
-
-	render() {
-		return (
-			<Html>
-				<Head />
-				<body>
-					<Main />
-					<NextScript />
-				</body>
-			</Html>
-		)
-	}
 }
