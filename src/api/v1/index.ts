152f50ce7ec75eeaa6e80f4d38715af28437bcd9
massaaki <eu@massaaki.com>
2022-06-13 09:52:23 -0300

    feat: add article request from api

--- src/api/v1/index.ts
+++ src/api/v1/index.ts
@@ -4,3 +4,11 @@ import axios from 'axios';
 export const api = axios.create({
 	baseURL: 'http://localhost:3000/api'
 })
+
+export const webSearchApi = axios.create({
+	baseURL: 'https://contextualwebsearch-websearch-v1.p.rapidapi.com/api/search/NewsSearchAPI',
+	headers: {
+		'X-RapidAPI-Key': process.env.RAPID_API_KEY || '',
+		'X-RapidAPI-Host': 'contextualwebsearch-websearch-v1.p.rapidapi.com'
+	}
+})
