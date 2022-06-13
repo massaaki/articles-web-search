f4f94b9c52f18b3595a08cb5defd8448efa46c6a
massaaki <eu@massaaki.com>
2022-06-13 13:44:39 -0300

    fix: adjust baseUrl switching between production and development

--- src/api/v1/index.ts
+++ src/api/v1/index.ts
@@ -2,7 +2,9 @@ import axios from 'axios';
 
 
 export const api = axios.create({
-	baseURL: 'http://localhost:3000/api'
+	baseURL: process.env.NODE_ENV === 'development'
+		? 'http://localhost:3000/api' :
+		'https://articles-web-search-xs5ihcowz-eu-massaakicom.vercel.app/api'
 })
 
 export const webSearchApi = axios.create({
@@ -12,7 +14,6 @@ export const webSearchApi = axios.create({
 		'X-RapidAPI-Host': 'contextualwebsearch-websearch-v1.p.rapidapi.com'
 	}
 })
-
 
 export const webSearchRecommendedApi = axios.create({
 	baseURL: 'https://contextualwebsearch-websearch-v1.p.rapidapi.com/api/spelling/AutoComplete',
