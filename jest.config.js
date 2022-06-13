aedc969a1208d5deaed4ae900d077a7bed120b90
massaaki <eu@massaaki.com>
2022-06-13 17:23:48 -0300

    test: remove coverage

--- jest.config.js
+++ jest.config.js
@@ -1,17 +1,6 @@
 module.exports = {
 	testEnvironment: 'jsdom',
 	testPathIgnorePatterns: ['/node_modules/', '/.next/'],
-	collectCoverage: true,
-	collectCoverageFrom: [
-		'src/**/*.ts(x)?',
-		'!src/**/stories.tsx',
-		'!src/pages/**/*.tsx',
-		'!src/styles/**/*.ts',
-		'!src/types/**/*.d.ts',
-		'!src/utils/test-utils.tsx',
-		'!src/icons/**/*.tsx',
-		'!src/context/**/*'
-	],
 	setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
 	modulePaths: ['<rootDir>/src/', '<rootDir>/.jest']
 }
