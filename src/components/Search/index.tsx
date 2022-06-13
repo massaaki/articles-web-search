07bec185c8e4f58efeb0adae6a45f9109511cbdf
massaaki <eu@massaaki.com>
2022-06-12 23:23:55 -0300

    feat: add recommended terms after debounce

--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -1,10 +1,10 @@
-import React, { useEffect, useMemo, useState } from 'react';
+import React, { useEffect, useMemo, useState, useRef } from 'react';
 import debounce from 'lodash.debounce';
 
 import { api } from 'api/v1';
 import { Article } from 'interfaces/article';
-import { ArticleList } from 'components/ArticleList';
 
+import { ArticleList } from 'components/ArticleList';
 
 export type ArticleResponse = {
 	data:  {
@@ -12,20 +12,31 @@ export type ArticleResponse = {
 	}
 }
 
+export type RecommendedTerms = {
+	data: string[]
+}
+
 
 export const Search = () => {
 	const [termToSearch, setTermToSearch] = useState('');
 	const [articles, setArticles] = useState<Article[]>();
+	const [recommendedTerms, setRecommendedTerms] = useState<string[]>([]);
+	const inputSearchRef = useRef<HTMLInputElement>(null);
 
 	const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-		console.log('recommended items');
 		setTermToSearch(e.target.value);
+		if (e.target.value)
+			fetchAutocomplete();
 	}
 
 	const debounceSearchResults = useMemo(() => {
 		return debounce(handleChange, 500);
 	}, []);
 
+	const fetchAutocomplete = async () => {
+		const response: RecommendedTerms = await api.get('/autocomplete?text=asd');
+		setRecommendedTerms(response.data);
+	}
 
 	const fetch = async () => {
 		const response: ArticleResponse = await api.get('/articles?search=asd');
@@ -34,21 +45,30 @@ export const Search = () => {
 
 	useEffect(() => {
 		return () => {
-			debounceSearchResults.cancel();
+			if (debounceSearchResults)
+				debounceSearchResults.cancel();
 		}
 	})
 
 	useEffect(() => {
-		console.log(articles);
-	}, [articles])
+		if (!termToSearch) {
+			setRecommendedTerms([]);
+		}
+	}, [termToSearch])
 
 
 
 	return (
 		<>
 			<div>
-				<input type="text" onChange={debounceSearchResults} />
+				<input ref={inputSearchRef} type="text" onChange={debounceSearchResults} />
 				<button onClick={() => fetch()}>Search</button>
+				{recommendedTerms && (
+					<ul>
+						{recommendedTerms.map(term => <li key={term}>{term}</li>)}
+					</ul>
+				)}
+
 			</div>
 			{articles && (<ArticleList articles={articles}/>)}
 		</>
