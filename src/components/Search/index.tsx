8414623c1b4e7549e8f45213aecb37976a69946c
massaaki <eu@massaaki.com>
2022-06-12 22:54:04 -0300

    feat: add axios request and button to fetch request

--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -1,11 +1,24 @@
 import React, { useEffect, useMemo, useState } from 'react';
 import debounce from 'lodash.debounce';
 
+import { api } from 'api/v1';
+import { Article } from 'interfaces/article';
+import { ArticleList } from 'components/ArticleList';
+
+
+export type ArticleResponse = {
+	data:  {
+		value: Article[]
+	}
+}
+
+
 export const Search = () => {
 	const [termToSearch, setTermToSearch] = useState('');
+	const [articles, setArticles] = useState<Article[]>();
 
 	const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-		console.log('call api!');
+		console.log('recommended items');
 		setTermToSearch(e.target.value);
 	}
 
@@ -13,15 +26,31 @@ export const Search = () => {
 		return debounce(handleChange, 500);
 	}, []);
 
+
+	const fetch = async () => {
+		const response: ArticleResponse = await api.get('/articles?search=asd');
+		setArticles(response.data.value);
+	}
+
 	useEffect(() => {
 		return () => {
 			debounceSearchResults.cancel();
 		}
 	})
 
+	useEffect(() => {
+		console.log(articles);
+	}, [articles])
+
+
+
 	return (
 		<>
-			<input type="text" onChange={debounceSearchResults} />
+			<div>
+				<input type="text" onChange={debounceSearchResults} />
+				<button onClick={() => fetch()}>Search</button>
+			</div>
+			{articles && (<ArticleList articles={articles}/>)}
 		</>
 	)
 }
