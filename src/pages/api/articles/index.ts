0f5712eedc4f52bbb7fb297346892c16c0030458
massaaki <eu@massaaki.com>
2022-06-13 09:56:13 -0300

    feat: add api request from RapitApi

--- src/pages/api/articles/index.ts
+++ src/pages/api/articles/index.ts
@@ -1,10 +1,17 @@
-import {NextApiRequest, NextApiResponse} from 'next'
+import { NextApiRequest, NextApiResponse } from 'next'
 
-import fakeResult from '../../../mocks/mock-fake-news-result.json';
+import { webSearchApi } from 'api/v1';
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
 	const { search } = req.query;
 	if (req.method === 'GET' && search) {
-		res.status(200).json(fakeResult);
+
+		const options = {
+			method: 'GET',
+			params: { q: search, pageNumber: '1', pageSize: '10', autoCorrect: 'true' },
+		};
+
+		const response = await webSearchApi.request(options);
+		res.status(200).json(response.data);
 	}
 }
