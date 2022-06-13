7a51290f761abaeb8abf8c104f40893c652241ad
massaaki <eu@massaaki.com>
2022-06-13 13:25:02 -0300

    fix: adjust storybook elements to build

--- src/components/ArticleList/stories.tsx
+++ src/components/ArticleList/stories.tsx
@@ -1,10 +1,10 @@
 import { Story, Meta } from '@storybook/react/types-6-0';
 import { ArticleList } from '.';
-
+import {article} from '../ArticleItem/mock';
 
 export default {
 	title: 'ArticleList',
 	component: ArticleList
 } as Meta;
 
-export const Basic: Story = () => <ArticleList />
+export const Basic: Story = () => <ArticleList  articles={[article]}/>
