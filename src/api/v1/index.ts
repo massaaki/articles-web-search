98422c63df7ac30ff1a771e1f31f36c8dc8776b7
massaaki <eu@massaaki.com>
2022-06-13 13:58:55 -0300

    fix: allow api routes on server

--- src/api/v1/index.ts
+++ src/api/v1/index.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 export const api = axios.create({
 	baseURL: process.env.NODE_ENV === 'development'
-		? 'http://localhost:3000/api' :
+		? 'https://articles-web-search-xs5ihcowz-eu-massaakicom.vercel.app/api' :
 		'https://articles-web-search-xs5ihcowz-eu-massaakicom.vercel.app/api'
 })
 
--- src/pages/api/articles/index.ts
+++ src/pages/api/articles/index.ts
@@ -2,7 +2,28 @@ import { NextApiRequest, NextApiResponse } from 'next'
 
 import { webSearchApi } from 'api/v1';
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+
+
+const allowCors = (fn: { (req: NextApiRequest, res: NextApiResponse<any>): Promise<void>; (arg0: any, arg1: any): any; }) => async (req: any, res: any) => {
+	res.setHeader('Access-Control-Allow-Credentials', true)
+	res.setHeader('Access-Control-Allow-Origin', '*')
+	// another common pattern
+	// res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+	res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+	res.setHeader(
+		'Access-Control-Allow-Headers',
+		'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+	)
+	if (req.method === 'OPTIONS') {
+		res.status(200).end()
+		return
+	}
+	return await fn(req, res)
+}
+
+
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
 	const { search } = req.query;
 	if (req.method === 'GET' && search) {
 
@@ -15,3 +36,5 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 		res.status(200).json(response.data);
 	}
 }
+
+module.exports = allowCors(handler)
