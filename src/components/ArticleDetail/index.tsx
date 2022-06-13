fdb6d20cee8cde88f6b6116132e8d1cdee5022f7
massaaki <eu@massaaki.com>
2022-06-13 00:38:02 -0300

    chore: add react-tagcloud

--- src/components/ArticleDetail/index.tsx
+++ src/components/ArticleDetail/index.tsx
@@ -1,7 +1,22 @@
-import { article } from "components/ArticleItem/mock";
-import { ArticleContext } from "context/articleContext";
+
 import Link from "next/link";
 import { useContext } from "react";
+
+import { ArticleContext } from "context/articleContext";
+import { TagCloud } from 'react-tagcloud';
+
+import { article } from "components/ArticleItem/mock";
+
+
+const data = [
+	{ value: 'JavaScript', count: 38 },
+	{ value: 'React', count: 30 },
+	{ value: 'Nodejs', count: 28 },
+	{ value: 'Express.js', count: 25 },
+	{ value: 'HTML5', count: 33 },
+	{ value: 'MongoDB', count: 18 },
+	{ value: 'CSS3', count: 20 },
+]
 
 export const ArticleDetail = () => {
 	const { currentArticle } = useContext(ArticleContext);
@@ -13,6 +28,13 @@ export const ArticleDetail = () => {
 		<>
 			<h2>Article Detail</h2>
 			<p>{article.snippet}</p>
+			<div style={{'maxWidth': '500px'}}>
+			<TagCloud
+				minSize={12}
+				maxSize={35}
+				tags={data}
+				/>
+			</div>
 
 			<Link href='/'>
 				<a>
