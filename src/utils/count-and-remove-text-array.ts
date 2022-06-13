87af16e06ebcf36093c6db8e8c3634e3b94ea39b
massaaki <eu@massaaki.com>
2022-06-13 10:26:52 -0300

    feat: add count and remove duplicated helper function

---
+++ src/utils/count-and-remove-text-array.ts
@@ -0,0 +1,33 @@
+
+type ResponseType = {
+	value: string;
+	count: number;
+}[]
+
+export const CountAndRemoveDuplicatedString = (text: string): ResponseType => {
+	try {
+	const array = text.split(' ');
+
+	const results = array.reduce((r: { [key: string]: number }, s) => {
+		const key = s[0].toUpperCase() + s.substring(1).toLowerCase();
+
+		r[key] = (r[key] || 0) + 1;
+
+		return r;
+	}, {});
+
+	const keys = Object.keys(results);
+	const counts = Object.values(results);
+
+	const result = keys.map((key, index) => ({
+		value: key,
+		count: counts[index]
+	}))
+
+
+
+		return result;
+	} catch {
+		return []
+	}
+}
