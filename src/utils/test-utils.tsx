34aa44e9897d32c5e831f643a6e9ada49a06af66
massaaki <eu@massaaki.com>
2022-06-13 16:21:51 -0300

    test: add context as a propertie in render helper

--- src/utils/test-utils.tsx
+++ src/utils/test-utils.tsx
@@ -1,22 +1,37 @@
 import { render, RenderOptions } from '@testing-library/react'
 import { ReactElement } from 'react'
 import { ThemeProvider } from 'styled-components'
+import {ArticleContextType, ArticleContext, ArticleProvider} from 'context/articleContext'
 import theme from 'styles/theme'
 
 type CustomRenderProps = Omit<RenderOptions, 'queries'>
 
 const customRender = (
 	ui: ReactElement,
+	providerProps?: ArticleContextType,
 	{
 		...renderOptions
 	}: CustomRenderProps = {}
 ) =>
-	render(
+
+	providerProps ?
+		render(
 		<ThemeProvider theme={theme}>
+			<ArticleContext.Provider value={providerProps}>
 				{ui}
+			</ArticleContext.Provider>
 		</ThemeProvider>,
 		renderOptions
-	)
+		) :
+		render(
+			<ThemeProvider theme={theme}>
+				<ArticleProvider>
+					{ui}
+				</ArticleProvider>
+			</ThemeProvider>,
+			renderOptions
+		)
+
 
 export * from '@testing-library/react'
 export { customRender as render }
